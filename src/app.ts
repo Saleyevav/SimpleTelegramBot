de4c2902900fe00fd7a206033bb19c2893bb35a6
Alexandr Saleyev <saleyevav@yandex.ru>
2023-10-12 14:55:03 +0300

    Add commands

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,35 @@
 import { bot } from "./modules/bot";
 
+const commands = [
+  {
+    command: "start",
+    description: "Запуск бота",
+  },
+
+  {
+    command: "help",
+    description: "Раздел помощи",
+  },
+];
+
+bot.setMyCommands(commands);
+
 bot.on("text", async (msg) => {
-  await bot.sendMessage(msg.chat.id, msg.text);
+  try {
+    if (msg.text == "/start") {
+      await bot.sendMessage(msg.chat.id, `Вы запустили бота!`);
+    } else if (msg.text == "/help") {
+      await bot.sendMessage(
+        msg.chat.id,
+        `Раздел помощи HTML\n\n<b>Жирный Текст</b>\n<i>Текст Курсивом</i>\n<code>Текст с Копированием</code>\n<s>Перечеркнутый текст</s>\n<u>Подчеркнутый текст</u>\n<pre language='c++'>код на c++</pre>\n<a href='t.me'>Гиперссылка</a>`,
+        {
+          parse_mode: "HTML",
+        }
+      );
+    } else {
+      await bot.sendMessage(msg.chat.id, msg.text);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
